64313450b388389ad5dde90e1c32bceac6d458dc
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-28 16:44:25 +0100

    experience front end and added rich text editor to body experience model

--- biomax/experience/models.py
+++ biomax/experience/models.py
@@ -1,8 +1,12 @@
 from multiprocessing.sharedctypes import Value
+from tkinter.tix import Tree
+from tokenize import blank_re
 from django.db import models
+from ckeditor.fields import RichTextField 
 
 # Create your models here.
 
 class Experience(models.Model):
     nom = models.CharField(max_length=200)
+    body = RichTextField(blank = True, null = True)
     created = models.DateTimeField(auto_now_add=True, blank=True)
--- biomax/experience/views.py
+++ biomax/experience/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import request
+from .models import Experience
 
 # Create your views here.
+
+def index(request):
+    experiences = Experience.objects.all()
+    return render(request,'experience/index.html',{'experiences' : experiences})
