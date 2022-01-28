a27445e6edbd5429280359c5027f640be942a85f
OPRTNL <46159142+OPRTNL@users.noreply.github.com>
2022-01-28 11:27:29 +0100

    added personnalised admin fields to Experience and created field to Experience model

--- biomax/experience/admin.py
+++ biomax/experience/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from .models import Experience
 
 # Register your models here.
 
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('nom','created')
+
+admin.site.register(Experience, ExperienceAdmin)
--- biomax/experience/models.py
+++ biomax/experience/models.py
@@ -1,6 +1,8 @@
+from multiprocessing.sharedctypes import Value
 from django.db import models
 
 # Create your models here.
 
 class Experience(models.Model):
     nom = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
